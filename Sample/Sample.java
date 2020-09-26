b9f0c44c7a1d96eb22472eede96cc8b5baa49b63
blogSoul <blo9040@gmail.com>
2020-09-26 23:34:51 +0900

    add acmicpc files

--- Sample/Sample.java
+++ Sample/Sample.java
@@ -2,38 +2,14 @@ import java.util.Scanner;
 
 public class Sample {
     public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        String[] str = scan.nextLine().split(" ");
-        int n = Integer.parseInt(str[0]);
-        int m = Integer.parseInt(str[1]);
-        int[][] arr = new int[n][m];
-        int i, j;
-        for(i = 0 ; i < n; i++){
-            str = scan.nextLine().split(" ");
-            for(j = 0 ; j < m; j++){
-                arr[i][j] = Integer.parseInt(str[j]);
-            }
+        String p = "()()((()))";
+        int i = 0, countfirst = 0;
+        for(i = 0; i < p.length(); i++){
+            if(p.charAt(i) == '(')
+                countfirst++;
+            else if(p.charAt(i)== ')')
+                countfirst++;
         }
-        /*
-        for(i = 0 ; i < n; i++){
-            for(j = 0 ; j < m; j++){
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.print("\n");
-        }
-        */
-        for(i = 1 ; i < n; i++){
-            arr[i][0] += arr[i - 1][0];
-        }
-        for(i = 1 ; i < m; i++){
-            arr[0][i] += arr[0][i - 1];
-        }
-        for(i = 1 ; i < n; i++){
-            for(j = 1 ; j < m; j++){
-                arr[i][j] += Math.max(arr[i][j - 1], Math.max(arr[i - 1][j - 1], arr[i - 1][j]));
-            }
-        }
-        System.out.println(arr[n - 1][m - 1]);
     }
 }
 /*
