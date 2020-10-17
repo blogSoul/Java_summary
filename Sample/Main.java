386284ac37294360d17fbec84496e8348a8b69c9
blogSoul <blo9040@gmail.com>
2020-10-17 23:21:43 +0900

    add acmicpc files

--- Sample/Main.java
+++ Sample/Main.java
@@ -1,19 +1,16 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception{
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K = sc.nextInt();
-        int[] value = new int[N];
-        int cnt = 0;
-        for(int i = 0; i < N; i++){
-            value[i] = sc.nextInt();
-        }
-        for(int i = N - 1; i >= 0; i--){
-            cnt += K / value[i];
-            K -= (K / value[i]) * value[i];
+        int i, N = sc.nextInt();
+        long[] arr = new long[N];
+        long min;
+        for(i = 0; i < N; i++){
+            arr[i] = sc.nextInt();
+            min = (i == 0) ? Math.min(min, arr[i]) : arr[i];
         }
         sc.close();
-        System.out.print(cnt);    
+        while
     }
 }
